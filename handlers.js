4f088842cc04d69c693b134d6013250a07d96957
samicelus <samicelus@hotmail.com>
2018-02-28 15:03:21 +0800

    release 20180228

--- handlers.js
+++ handlers.js
@@ -27,24 +27,73 @@ class bear_handlers{
         }
     }
 
-    chat_handler(data){
-        let content = data.content;
-        let from = data.from;
-        let type = data.type;
+    room_name_change_handler(data){
+        let new_room_name = data.new_room_name;
         let target_id = data.target_id;
-        let current_chat_id = $("#current_chat_name").val();
-        if(target_id == current_chat_id){
-            if(from == bear.get_user_id()){
-                $("#chat_page").append('<p><code style=\"float:right\">['+type+']'+content+'</code></p>')
-            }else{
-                $("#chat_page").append('<p><code>['+type+']'+content+'</code></p>')
+        $("#room_"+target_id).html(new_room_name);
+        if(target_id == window.current_room_id){
+            $("#room_name").val(new_room_name);
+        }
+    }
+
+    show_room_members_handler(data){
+        let members = data.members;
+        let target_id = data.target_id;
+        if(target_id == window.current_room_id){
+            $("#room_members_div").empty();
+            for(let user_id in members){
+                $("#room_members_div").append(
+                    '<div class="avatar" >' +
+                    '<img width="48" height="48" src="'+members[user_id].avatar+'" onerror="javascript:this.src=\'https://web.runningdoctor.cn/images/defaultHead.png\'"</img>' +
+                    '<div class="avatar_name">'+ members[user_id].nickname +'</div>' +
+                    '</div>'
+                );
             }
         }
     }
-};
 
-function affich(content){
-    $("#afficher").html($("#afficher").html()+ "</br>" + content);
+    chat_handler(data){
+        let user_id = this._client.get_user_id();
+        let from = data.from;
+        let from_user_id = data.from.user_id;
+        let position = "left";
+        if(from_user_id == user_id){
+            position = "right";
+        }
+        let content = data.content;
+        let url = data.url;
+        let type = data.type;
+        let target_id = data.target_id;
+        if(target_id == window.current_room_id){
+            switch(type){
+                case "text":
+                    if(position == "right"){
+                        $("#chat_page").append('<p style="text-align: '+position+'"><code>'+content+':['+from.nickname+']</code></p>');
+                    }else{
+                        $("#chat_page").append('<p style="text-align: '+position+'"><code>['+from.nickname+']:'+content+'</code></p>');
+                    }
+                    break;
+                case "image":
+                    if(position == "right"){
+                        $("#chat_page").append('<p style="text-align: '+position+'"><img width="304" height="228" src="'+url+'"></img><code>:['+from.nickname+']</code></p>');
+                    }else{
+                        $("#chat_page").append('<p style="text-align: '+position+'"><code>['+from.nickname+']:</code><img width="304" height="228" src="'+url+'"></img></p>');
+                    }
+                    break;
+                case "voice":
+                    if(position == "right"){
+                        $("#chat_page").append('<p style="text-align: '+position+'"><audio src="'+url+'"></audio><code>:['+from.nickname+']</code></p>');
+                    }else{
+                        $("#chat_page").append('<p style="text-align: '+position+'"><code>['+from.nickname+']:</code><audio src="'+url+'"></audio></p>');
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }else{
+
+        }
+    }
 }
 
 function refreshRooms(rooms){
